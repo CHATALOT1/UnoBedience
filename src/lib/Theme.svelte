3c955ba9d5a7ed8cf64a914ce5dcbc510025fa4b
Sylvia Mills <me@chatalot.xyz>
2022-06-12 00:48:26 +0100

    Add Theme.svelte

---
+++ src/lib/Theme.svelte
@@ -0,0 +1,10 @@
+<script>
+  import theme from "./theme.json";
+
+  const themeCss = `--primary-colour: ${theme.primary};\
+  --secondary-colour: ${theme.secondary}; --text-colour: ${theme.text};`;
+</script>
+
+<div id="theme-container" style={themeCss}>
+  <slot />
+</div>
