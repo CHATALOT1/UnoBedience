e66de0e63b6e4ae98c28f5e53fa673f9ec070b80
Sylvia Mills <me@chatalot.xyz>
2022-06-12 00:49:01 +0100

    Create basic layout

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,20 @@
+<script>
+  import Theme from "$lib/Theme.svelte";
+</script>
+
+<Theme>
+  <div id="layout-container">
+    <slot />
+  </div>
+</Theme>
+
+<style>
+  :global(body) {
+    margin: 0;
+  }
+
+  #layout-container {
+    height: 100vh;
+    background-color: var(--secondary-colour);
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,17 @@
-<h1>Coming Soon™</h1>
+<div id="container">
+  <h1>Coming Soon™</h1>
+</div>
+
+<style>
+  #container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 100%;
+  }
+
+  h1 {
+    text-align: center;
+    color: var(--primary-colour);
+  }
+</style>
